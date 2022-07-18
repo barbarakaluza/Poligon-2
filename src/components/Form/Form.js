68bfaf9137753b93d2e2e55b8fba744060143ebe
Borys <borys.kaluza@easystorage.io>
2022-07-18 13:20:09 +0200

    developing component Form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -9,12 +9,27 @@ import React from "react";
 // }
 
 class Form extends React.Component {
+  state = {
+    name: ""
+  };
+
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
   handlerButton = () => console.log("Click");
   handlerSubmit = (event) => event.preventDefault();
+  handleChange = (event) => {
+    console.log("input value: ", event.target.value)
+    this.setState({ name: event.target.value })
+    console.log("name: ", this.name);
+  }
 
   render() {
     return (
       <form onSubmit={this.handlerSubmit}>
+        <input type="text" onChange={this.handleChange}></input>
         <button onClick={this.handlerButton}>Click me!</button>
       </form>
     );
